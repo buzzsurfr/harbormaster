92997769b31cf901e21eac4fba91be4e5f5478b7
Theo Salvo <buzzsurfr@gmail.com>
2018-09-23 09:14:13 -0400

    Fix HelloWorld (incorporate test)

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -11,14 +9,9 @@ import (
 // is processed, it returns an Amazon API Gateway response object to AWS Lambda
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	index, err := ioutil.ReadFile("public/index.html")
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       string(index),
+		Body:       "<html><body>Congratulations</body></html>",
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},
