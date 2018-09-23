be214d8e893ac99e8edcf789db0f7a055d7b3e2c
Theo Salvo <buzzsurfr@gmail.com>
2018-09-23 15:48:29 -0400

    Add EKS to ClusterList

--- cluster/list/main.go
+++ cluster/list/main.go
@@ -35,6 +35,15 @@ func normalizeEcsCluster(ecsCluster *ecs.Cluster) Cluster {
 	}
 }
 
+func normalizeEksCluster(eksCluster *eks.Cluster) Cluster {
+	return Cluster{
+		Name:      *eksCluster.Name,
+		Arn:       *eksCluster.Arn,
+		Scheduler: "eks",
+		Status:    *eksCluster.Status,
+	}
+}
+
 func ecsListClusters(ctx context.Context) ([]Cluster, error) {
 	// ecs:ListClusters
 	resultListClusters, err := ecsSvc.ListClustersWithContext(ctx, &ecs.ListClustersInput{})
@@ -93,6 +102,68 @@ func ecsListClusters(ctx context.Context) ([]Cluster, error) {
 	return clusters, nil
 }
 
+func eksListClusters(ctx context.Context) ([]Cluster, error) {
+	// eks:ListClusters
+	resultListClusters, err := eksSvc.ListClustersWithContext(ctx, &eks.ListClustersInput{})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ecs.ErrCodeServerException:
+				log.Println(ecs.ErrCodeServerException, aerr.Error())
+			case ecs.ErrCodeClientException:
+				log.Println(ecs.ErrCodeClientException, aerr.Error())
+			case ecs.ErrCodeInvalidParameterException:
+				log.Println(ecs.ErrCodeInvalidParameterException, aerr.Error())
+			default:
+				log.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			log.Println(err.Error())
+		}
+		return nil, err
+	}
+
+	clusterNames := resultListClusters.Clusters
+
+	// eks:DescribeClusters (per cluster)
+	eksClusters := make([]eks.Cluster, len(clusterNames))
+	for i, clusterName := range clusterNames {
+		resultDescribeCluster, err := eksSvc.DescribeClusterWithContext(ctx, &eks.DescribeClusterInput{
+			Name: clusterName,
+		})
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				case ecs.ErrCodeServerException:
+					log.Println(ecs.ErrCodeServerException, aerr.Error())
+				case ecs.ErrCodeClientException:
+					log.Println(ecs.ErrCodeClientException, aerr.Error())
+				case ecs.ErrCodeInvalidParameterException:
+					log.Println(ecs.ErrCodeInvalidParameterException, aerr.Error())
+				default:
+					log.Println(aerr.Error())
+				}
+			} else {
+				// Print the error, cast err to awserr.Error to get the Code and
+				// Message from an error.
+				log.Println(err.Error())
+			}
+			return nil, err
+		}
+
+		eksClusters[i] = *resultDescribeCluster.Cluster
+	}
+
+	clusters := make([]Cluster, len(eksClusters))
+	for i, eksCluster := range eksClusters {
+		clusters[i] = normalizeEksCluster(&eksCluster)
+	}
+
+	return clusters, nil
+}
+
 // HandleRequest is the Lambda function handler
 func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// Lambda Context
@@ -104,18 +175,18 @@ func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (ev
 	xray.AWS(ecsSvc.Client)
 
 	// Initialize EKS
-	// eksSvc = eks.New(session.Must(session.NewSession()))
-	// xray.AWS(eksSvc.Client)
+	eksSvc = eks.New(session.Must(session.NewSession()))
+	xray.AWS(eksSvc.Client)
 
 	// List ECS Clusters
 	ecsClusters, _ := ecsListClusters(ctx)
 
 	// List EKS Clusters
-	// eksClusters, _ := eksListClusters(ctx)
+	eksClusters, _ := eksListClusters(ctx)
 
 	// Merge clusters from providers
-	clusters := ecsClusters
-	// clusters := append(ecsClusters, eksClusters...)
+	// clusters := ecsClusters
+	clusters := append(ecsClusters, eksClusters...)
 
 	responseBody, _ := json.Marshal(clusters)
 
