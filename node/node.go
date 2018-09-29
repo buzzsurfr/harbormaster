099a6a320261d03f02ee96811476a62a24f88aad
Theo Salvo <buzzsurfr@gmail.com>
2018-09-29 05:56:52 -0400

    Change serialization keys to lowercase from CamelCase

--- node/node.go
+++ node/node.go
@@ -4,10 +4,10 @@ import "github.com/buzzsurfr/harbormaster/cluster"
 
 // Node contains data for the normalized container instance/node
 type Node struct {
-	Name       string `locationName:"name" type:"string"`
-	Arn        string `locationName:"arn" type:"string"`
-	InstanceID string `locationName:"instanceId" type:"string"`
-	Scheduler  string `locationName:"scheduler" type:"string"`
-	Status     string `locationName:"status" type:"string"`
+	Name       string `json:"name" locationName:"name" type:"string"`
+	Arn        string `json:"arn" locationName:"arn" type:"string"`
+	InstanceID string `json:"instanceId" locationName:"instanceId" type:"string"`
+	Scheduler  string `json:"scheduler" locationName:"scheduler" type:"string"`
+	Status     string `json:"status" locationName:"status" type:"string"`
 	Cluster    cluster.Cluster
 }
